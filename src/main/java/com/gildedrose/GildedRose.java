3ed1acfee8cc4d72fe8fa32f121a2cbfc2c708d7
Artem Onufrei <onufrei.artem@gmail.com>
2021-10-25 19:00:16 +0300

    fixed 2

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -52,4 +52,54 @@ class GildedRose {
             }
         }
     }
+
+    private static final String AGEDBRIE = "Aged Brie";
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
+    public void updateQualityV2() {
+        for (Item item: items) {
+            if (!item.name.equals(AGEDBRIE) && !item.name.equals(BACKSTAGE)) {
+                if (item.quality > 0 && !item.name.equals(SULFURAS)) {
+                    item.quality--;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality++;
+
+                    if (item.name.equals(BACKSTAGE)) {
+                        if (item.sellIn < 11 && item.quality < 50) {
+                            item.quality++;
+                        }
+
+                        if (item.sellIn < 6 && item.quality < 50) {
+                            item.quality++;
+                        }
+                    }
+                }
+            }
+
+            if (!item.name.equals(SULFURAS)) {
+                item.sellIn--;
+            }
+
+            if (item.sellIn < 0) {
+                if (item.name.equals(AGEDBRIE)) {
+                    if (item.quality < 50) {
+                        item.quality++;
+                    }
+                } else {
+                    if (item.name.equals(BACKSTAGE)) {
+                        item.quality = 0;
+                    } else {
+                        if (item.quality > 0) {
+                            if (!item.name.equals(SULFURAS)) {
+                                item.quality--;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
